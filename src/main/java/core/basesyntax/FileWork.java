653e590a0f397bdc94d083b0af672d7587df9d3a
sofiia <sofiia.khapal@nure.ua>
2025-09-05 16:58:27 +0200

    edit style

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,19 +1,18 @@
 package core.basesyntax;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
-import java.util.stream.Stream;
 
 public class FileWork {
     public String[] readFromFile(String fileName) {
         StringBuilder answer = new StringBuilder();
-        try(BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line;
-            while((line = reader.readLine()) != null) {
+            while ((line = reader.readLine()) != null) {
                 String[] words = line.split(("\\s+"));
-                for(String word : words) {
+                for (String word : words) {
                     String cleaned = word.replaceAll("[^a-zA-Z]", "").toLowerCase();
                     if (!cleaned.isEmpty() && cleaned.charAt(0) == 'w') {
                         answer.append(cleaned).append(" ");
